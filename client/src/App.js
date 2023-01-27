2212ef4f096c343ca6b7db70c8cadf90f78e50c2
Bui <e2037519@cmaisonneuve.qc.ca>
2023-01-27 14:59:59 -0500

    penis

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className='uppercase text-6xl'>Navbar</div>
+  )
+}
+
