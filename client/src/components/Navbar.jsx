4db8e044f16453a0b91c4fd5bd2e5948d858ea77
tonybbb2 <89655535+tonybbb2@users.noreply.github.com>
2023-02-04 17:14:03 -0500

    expandable men navbar --non-responsive

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,15 +11,20 @@ export const Navbar = () => {
 
     const [scroll, setScroll] = useState(false)
     const [nav, setNav] = useState(false)
+    const [expand, setExpand] = useState(false)
 
     const handleNav = () => {
         setNav(!nav);
     }
 
+    const handleExpand = () => {
+        setExpand(!expand);
+    }
+
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > 20) {
+            if (window.scrollY > 70) {
                 setScroll(true)
             } else {
                 setScroll(false)
@@ -30,28 +35,39 @@ export const Navbar = () => {
 
 
     return (
-        <div className='flex justify-center'>
-            <div className={scroll ? 'flex justify-between opacity-90 items-center mx-auto md:w-2/3 h-10 fixed z-[100] top-0 text-slate-200 rounded-b-[2rem] bg-black  px-16 border border-slate-400' : 'flex justify-between items-center mx-auto md:w-2/3 h-16 fixed z-[100] top-0 text-slate-200 rounded-b-[2rem] bg-black px-16 border border-slate-400'} >
-                <div>
-                    <p className='uppercase font-normal text-[18px] md:text-xl font-sans tracking-tight italic'>FITNESS RATS</p>
-                </div>
-                <div className='md:flex px-2 hidden md:block mr-[50px] font-extrabold text-gray-300'>
-                    <p className='px-2 uppercase text-sm'>Men</p>
-                    <p className='px-2 uppercase text-sm'>Women</p>
-                </div>
-                <div className='flex'>
-                    <BsSearch className='text-lg md:text-2xl mr-2 hidden md:block' />
-                    <AiOutlineShoppingCart className='text-lg md:text-2xl mr-2 hidden md:block' />
-                    <CgProfile className='text-lg md:text-2xl hidden md:block' />
-                    {
-                        nav ? <AiOutlineClose onClick={handleNav} className='text-3xl ml-2 md:hidden' /> : <AiOutlineMenu onClick={handleNav} className='text-3xl ml-2 md:hidden' />
-                    }
+        <div>
+            <div className='w-full h-6 bg-black mx-auto md:w-2/3 top-0 text-slate-200 border-[1px] border-[#3d3d3d] border-y-[#3d3d3d] hidden md:block ' >
+                <div className='flex justify-between px-14 py-[2px]'>
+                    <div>
+                        <p className='text-[10px] font-bold text-gray-300 py-[1px]'>Free Shipping on all orders over $100</p>
+                    </div>
+                    <div>
+                        <p className='text-[12px] font-bold text-gray-300'>Login</p>
+                    </div>
                 </div>
             </div>
-            <div className={nav ? 'md:hidden fixed w-full inset-x-0 bottom-0  bg-black/80 z-[100]' : ''}>
-                <div className={nav ? ' fixed inset-x-0 bottom-0 overflow-y-scroll scroll-smooth scrollbar-hide mx-auto rounded-t-[50px] border-[1px] backdrop-blur-sm border-slate-500 w-[97%] h-[80%] bg-opacity-90 bg-black p-10 ease-in duration-500' :
-                    'hidden ease-in duration-500'
-                }>
+            <div className='flex justify-center'>
+                <div className={scroll ? 'flex justify-between opacity-90 fixed items-center mx-auto md:w-2/3 h-10 z-[100] top-0  text-slate-200 rounded-b-[2rem] bg-black  px-16 border border-slate-400' : 'flex justify-between items-center mx-auto md:w-2/3 h-16  z-[100] top-0 text-slate-200 rounded-b-[2rem] border-[1px] border-[#3d3d3d] bg-black px-16 '} >
+                    <div>
+                        <p className='uppercase font-normal text-[18px] md:text-xl font-sans tracking-tight italic'>FITNESS RATS</p>
+                    </div>
+                    <div className='md:flex px-2 hidden md:block mr-[50px] font-extrabold text-gray-300'>
+                        <p className='-ml-[40px] uppercase text-sm' onMouseOver={handleExpand}>Men</p>
+                        <p className='ml-4 uppercase text-sm' >Women</p>
+                    </div>
+                    <div className='flex'>
+                        <BsSearch className='text-lg md:text-xl mr-2 hidden md:block' />
+                        <AiOutlineShoppingCart className='text-lg md:text-xl mr-2 hidden md:block' />
+                        <CgProfile className='text-lg md:text-2xl hidden' />
+                        {
+                            nav ? <AiOutlineClose onClick={handleNav} className='text-3xl ml-2 md:hidden' /> : <AiOutlineMenu onClick={handleNav} className='text-3xl ml-2 md:hidden' />
+                        }
+                    </div>
+                </div>
+                <div className={nav ? 'md:hidden fixed w-full inset-x-0 bottom-0  bg-black/80 z-[100]' : ''}>
+                    <div className={nav ? ' fixed inset-x-0 bottom-0 overflow-y-scroll scroll-smooth scrollbar-hide mx-auto rounded-t-[50px] border-[1px] backdrop-blur-sm border-slate-500 w-[97%] h-[80%] bg-opacity-90 bg-black p-10 ease-in duration-500' :
+                        'hidden ease-in duration-500'
+                    }>
                         {/* <div className='flex w-full items-center justify-between'>
                             <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                                 <AiOutlineClose size={25} />
@@ -181,9 +197,96 @@ export const Navbar = () => {
                                 </div>
                             </div>
                         </form>
-                </div>
+                    </div>
 
+                </div>
             </div>
+            {expand ?
+                <div onMouseLeave={handleExpand} className='fixed inset-x-0 top-[64px] h-[28rem] z-[100] border-x-[1px] border-[#3d3d3d] mx-auto bg-opacity-90 bg-black hidden md:block md:w-2/3 text-slate-200 rounded-b-[2rem]' >
+                    <hr class="h-px mx-auto my-8 w-[15rem] sm:w-[75rem] bg-gray-400 border-0"></hr>
+                    <div className='grid grid-cols-2'>
+                        <div className='grid grid-rows-4 text-center'>
+                            <div className='row-span-3'>
+                                <p className='text-md font-bold uppercase'>products</p>
+                                <div className='grid grid-cols-2'>
+                                    <div>
+                                        <ul class="space-y-2 text-slate-300 list-none text-left list-inside text-md p-10">
+                                            <li>
+                                                Shorts
+                                            </li>
+                                            <li>
+                                                Joggers
+                                            </li>
+                                            <li>
+                                                Short Sleeves
+                                            </li>
+                                            <li>
+                                                Tank Tops
+                                            </li>
+
+                                        </ul></div>
+                                    <div><ul class="space-y-2 text-slate-300 list-none   text-left list-inside text-md p-10">
+                                        <li>
+                                            Shirts & Crops
+                                        </li>
+                                        <li>
+                                            Hoodies & Jackets
+                                        </li>
+                                        <li>
+                                            Joggers
+                                        </li>
+                                        <li>
+                                            Dresses & Skirts
+                                        </li>
+
+                                    </ul></div>
+                                </div>
+                            </div>
+                            <div><hr class="h-px mx-auto w-[15rem] sm:w-[25rem] bg-gray-400 border-0"></hr>
+                                <div className='p-8 flex justify-between text-white text-2xl '>
+                                    <CgProfile className=' mr-2' />
+                                    <FiHelpCircle className='mr-2' />
+                                    <FaTruckLoading className='mr-2' />
+                                    <GiReturnArrow className='mr-2' />
+                                    <FaShoppingBag className='mr-2' />
+                                    <BsFillInfoCircleFill className=' mr-2' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='grid grid-cols-2'>
+                            <div>
+                                <div className='text-center'>
+                                    <div className='w-[150px] sm:w-[275px] inline-block relative'>
+                                        <img className='w-full h-[345px] block object-cover rounded-lg' src='https://cdn.shopify.com/s/files/1/0667/0133/files/11BFHaze_19.jpg?crop=center&height=300&v=1670864224&width=250' alt='tkt' />
+                                        <div className='absolute top-0 left-0 w-full h-full text-black'>
+                                            <div className='absolute top-4 left-4'>
+                                                <button className="bg-gray-500 bg-opacity-80 text-white text-[12px] font-extrabold py-1 px-3 rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mt-2">
+                                                    New Arrivals
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div className='text-center'>
+                                    <div className='w-[150px] sm:w-[275px] inline-block relative'>
+                                        <img className='w-full h-auto block object-cover rounded-lg' src='https://cdn.shopify.com/s/files/1/0538/2658/4736/files/01BF-NEW_042-port_800x.jpg?v=1668864373' alt='tkt' />
+                                        <div className='absolute top-0 left-0 w-full h-full text-black'>
+                                            <div className='absolute top-4 left-4'>
+                                                <button className="bg-gray-500 bg-opacity-80 text-white text-[12px] font-extrabold py-1 px-3 rounded-full transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 mt-2">
+                                                    All Men
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                :
+                false}
         </div>
     )
 }
