f589fb12fc262bb5625845ad8527b5f92ec7011e
Antoine <e1939748@cmaisonneuve.qc.ca>
2023-02-08 13:42:24 -0500

    Auto stash before merge of "main" and "origin/main"

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,10 +1,18 @@
 import React from "react"
+import CartCard from "./components/CartCard"
 
 function Cart() {
 
     return (
+<<<<<<< Updated upstream:client/src/components/Cart.jsx
         <>
         </>
+=======
+        <div>
+            <CartCard />
+        </div>
+
+>>>>>>> Stashed changes:client/src/Cart.jsx
     )
 }
 
