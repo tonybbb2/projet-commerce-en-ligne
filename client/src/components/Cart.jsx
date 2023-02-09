24beda4d84dd4768566c16c3ba50a00f1b6482a2
Antoine <e1939748@cmaisonneuve.qc.ca>
2023-02-09 17:04:05 -0500

    Page Cart

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,18 +1,37 @@
 import React from "react"
-import CartCard from "./components/CartCard"
+import CartCard from "./CartCard"
 
 function Cart() {
 
     return (
-<<<<<<< Updated upstream:client/src/components/Cart.jsx
-        <>
-        </>
-=======
-        <div>
-            <CartCard />
+        <div >
+            <div className="xl:w-8/12 lg:w-11/12 mx-auto mt-10">
+                <h1 className="text-white font-extrabold text-3xl">YOUR BAG</h1>
+            </div>
+            <div className='xl:w-8/12 lg:w-11/12 h-16 rounded-md bg-black mx-auto mt-4 text-slate-200 border-[1px] border-[#3d3d3d] border-y-[#3d3d3d] hidden md:block' >
+                <div className='flex justify-between px-14 py-[2px]'>
+                    <div className="py-4">
+                        <p className='text-lg font-bold text-gray-300'>TOTAL: 112.00 USD</p>
+                    </div>
+                    <div className="py-2">
+                        <button id="checkout-btn" className="rounded-md border-2 border-green-500 bg-green-500 hover:bg-green-600" type="submit" name="checkout">
+                            <p className="font-extrabold mx-6 my-2">CHECKOUT</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-row xl:flex-row lg:flex-row sm:flex-col justify-center">
+                <div className="flex flex-col mx-10 my-10">
+                    <CartCard title={'OUTLINED WOLF HEAD DAD HAT'} color={'White'} size={'Adjustable'} pricing={'28.00'} cover={'https://cdn.shopify.com/s/files/1/0538/2658/4736/products/WhiteWolfHeadHat2_f175ba50-4906-40af-966a-3bd25c8e535e_1000x.jpg?v=1673718721'} quantity={'1'} />
+                    <CartCard title={'OUTLINED WOLF HEAD DAD HAT'} color={'White'} size={'Adjustable'} pricing={'28.00'} cover={'https://cdn.shopify.com/s/files/1/0538/2658/4736/products/WhiteWolfHeadHat2_f175ba50-4906-40af-966a-3bd25c8e535e_1000x.jpg?v=1673718721'} quantity={'1'} />
+                </div>
+                <div className="flex flex-col mx-10 my-10">
+                    <CartCard title={'OUTLINED WOLF HEAD DAD HAT'} color={'White'} size={'Adjustable'} pricing={'28.00'} cover={'https://cdn.shopify.com/s/files/1/0538/2658/4736/products/WhiteWolfHeadHat2_f175ba50-4906-40af-966a-3bd25c8e535e_1000x.jpg?v=1673718721'} quantity={'1'} />
+                    <CartCard title={'OUTLINED WOLF HEAD DAD HAT'} color={'White'} size={'Adjustable'} pricing={'28.00'} cover={'https://cdn.shopify.com/s/files/1/0538/2658/4736/products/WhiteWolfHeadHat2_f175ba50-4906-40af-966a-3bd25c8e535e_1000x.jpg?v=1673718721'} quantity={'1'} />
+                </div>
+            </div>
         </div>
 
->>>>>>> Stashed changes:client/src/Cart.jsx
     )
 }
 
--- client/src/components/CartCard.jsx
+++ client/src/components/CartCard.jsx
@@ -8,18 +8,18 @@ const CartCard = ({ title, color, size, pricing, cover, quantity }) => {
     return (
         <div>
             <div className="flex flex-row">
-                <article className="flex flex-col rounded-[1.5rem] p-[2rem] pb-[1rem] border border-secondary/20 shadow-xl transition-all duration-[1s] ease-out-expo active:scale-95">
+                <article className="flex flex-col rounded-[1.5rem] p-[2rem] pb-[1rem] border border-secondary/20 shadow-xl transition-all duration-[1s] ease-out-expo active:scale-95 h-60 my-5 border-gray-700">
                     <div className="flex flex-row">
                         <div className="flex flex-col">
                             <div className="grid gap-[5px]">
-                                <p className="text-white text-[1.3rem] font-bold" rv-text="item.product_title | productTitle">{title}</p>
-                                <p className="text-white text-[1.1rem] opacity-60" rv-text="item.product_title | productColorname">{color}</p>
-                                <span className="text-white text-[1.1rem] opacity-60" rv-text="item.variant_title">{size}</span>
+                                <p className="text-white text-xl font-bold" rv-text="item.product_title | productTitle">{title}</p>
+                                <p className="text-white text-base opacity-60" rv-text="item.product_title | productColorname">{color}</p>
+                                <span className="text-white text-base opacity-60" rv-text="item.variant_title">{size}</span>
                             </div>
                             <span className="text-white mt-auto font-bold" rv-html="item.price | money Currency.currentCurrency">{pricing} USD</span>
                         </div>
                         <div className="ml-20">
-                            <img className="lazyload-fade absolute-tl wh-full-cover rounded-[1rem] ls-is-cached lazyloaded" rv-src="item.image | product_image_size 'medium'" src={cover}></img>
+                            <img className="lazyload-fade absolute-tl wh-full-cover rounded-[1rem] ls-is-cached lazyloaded h-32" rv-src="item.image | product_image_size 'medium'" src={cover}></img>
                         </div>
                     </div>
                     <div className="w-full flex items-center justify-between pt-[10px] mt-[10px] text-[1.2rem] border-t border-secondary/20">
