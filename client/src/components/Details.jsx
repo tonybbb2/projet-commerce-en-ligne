b5c8a3c3e3343b7d417fcc261dfddc90bada2653
Marc Charlebois <CharleboisMarc@hotmail.com>
2023-02-13 17:15:34 -0500

    Fix requetes images

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -13,29 +13,27 @@ function Details() {
     }
 
     useEffect(() => {
-        const getProductDetail = async () => {
-            // Function to fetch and set the product detail
-            const clothesRef = doc(db, 'clothes/urls')
+        // Function to fetch and set the product detail
+        const clothesRef = doc(db, 'clothes/urls')
 
-            const fetchClothes = async () => {
-                // const braPath = doc(clothesRef, 'Bra_small_icon')
-                const bra = await getDoc(clothesRef)
-                if (bra.exists()) {
-                    return ('Document data:', bra.data());
-                } else {
-                    return ('No such document!');
-                }
+        const fetchClothes = async () => {
+            //const braPath = doc(clothesRef, 'Bra_small_icon')
+            const bra = await getDoc(clothesRef)
+            if (bra.exists()) {
+                return ('Document data:', bra.data());
+            } else {
+                return ('No such document!');
             }
-
-            const result = fetchClothes()
-                .catch(console.error)
-            result.then(value => {
-                console.log(value.Bra_big_image)
-            })
-
         }
 
-        getProductDetail();
+        const result = fetchClothes()
+            .catch(console.error)
+        result.then(value => {
+            console.log(value)
+            setProduct(value.Bra_big_image)
+        })
+
+
     }, [])
 
     return (
@@ -43,6 +41,43 @@ function Details() {
             <div className="container px-5 py-24 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
                     <img alt="Product Image" className="lg:w-1/2 w-full object-cover object-center rounded-[20px] border" src="https://cdn.shopify.com/s/files/1/0538/2658/4736/products/AmplifyRestockMay310083_1000x.jpg?v=1673719047" />
+                    <div id="default-carousel" className="relative" data-carousel="static">
+                        Carousel wrapper
+                        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+                            {
+                                product &&
+                                product.map(imageUrl => {
+                                    console.log(imageUrl.replace(/\s/g, ''))
+                                    return (
+                                        <div className="hidden duration-700 ease-in-out" data-carousel-item key={imageUrl.replace(/\s/g, '')}>
+                                            <img src={imageUrl.replace(/\s/g, '')} className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        Slider indicators
+                        <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+                            <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+                        </div>
+                        Slider controls
+                        <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                            <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
+                                <span className="sr-only">Previous</span>
+                            </span>
+                        </button>
+                        <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                            <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                                <span className="sr-only">Next</span>
+                            </span>
+                        </button>
+                    </div>
+
+
                     <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
                         <h1 className="text-gray-300 text-3xl title-font font-medium mb-5">PRODUCT NAME</h1>
@@ -81,6 +116,13 @@ function Details() {
                     </div>
                 </div>
             </div>
+
+            {/* {
+                product &&
+                product.map(imageUrl => {
+                    return <img src={imageUrl.replace(/\s/g, '')} key={imageUrl} />
+                })
+            } */}
         </section>
     )
 }
