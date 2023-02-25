8717796901c471f8c37e32f51286067997d1e737
Marc Charlebois <CharleboisMarc@hotmail.com>
2023-02-25 16:27:16 -0500

    Update data men

--- client/src/data.js
+++ client/src/data.js
@@ -33,6 +33,142 @@ const data = [
             },
         ]
     },
+    {
+        title: "STUDIO PANTS",
+        description: "Men's Lightweight 4-Way Stretch 29inch Training Pants",
+        type: "joggers",
+        gender: "men",
+        price: 64.00,
+        sizes: ["S", "M", "L", "XL"],
+        colorways: [
+            {
+                color: "Fog Blue",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/FogBlueStudioPant5_1000x.jpg?v=1667952678",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/FogBlueStudioPant2_1000x.jpg?v=1667952677",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/FogBlueStudioPant5_150x.jpg?v=1667952678"
+            },
+            {
+                color: "Black",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/BlackStudioPants5_1000x.jpg?v=1667952640",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/BlackStudioPants4_1000x.jpg?v=1667952640",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/BlackStudioPants5_150x.jpg?v=1667952640"
+            },
+            {
+                color: "Oyster Grey",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/OysterGreyStudioPant6_1000x.jpg?v=1667952882",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/OysterGreyStudioPant2_1000x.jpg?v=1667952882",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/OysterGreyStudioPant6_150x.jpg?v=1667952882"
+            },
+            {
+                color: "Whale Blue",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/WhaleBlueStudioPant1_1000x.jpg?v=1667953108",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/WhaleBlueStudioPant3_1000x.jpg?v=1667953106",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/WhaleBlueStudioPant1_150x.jpg?v=1667953108"
+            },
+        ]
+    },
+    {
+        title: "DYNASTY TANKS",
+        description: "Men's Fitted Performance Tank",
+        type: "tanks",
+        gender: "men",
+        price: 32.00,
+        sizes: ["S", "M", "L", "XL"],
+        colorways: [
+            {
+                color: "Solid Pink",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/MoandIlon25_1_1000x.jpg?v=1657309475",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/MoandIlon27_1000x.jpg?v=1657309476",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/MoandIlon25_1_150x.jpg?v=1657309475"
+            },
+            {
+                color: "Black",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/MoandIlon1_1000x.jpg?v=1657309240",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/MoandIlon3_1000x.jpg?v=1657309240",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/MoandIlon1_150x.jpg?v=1657309240"
+            },
+            {
+                color: "Congo Brown",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_ba548afe-ed1e-44e6-a0c7-c9fd41d8fb46_1000x.jpg?v=1657321637",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_fbf87303-da54-41bf-b2c9-2efa84abbd42_1000x.jpg?v=1657321641",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/image_ba548afe-ed1e-44e6-a0c7-c9fd41d8fb46_150x.jpg?v=1657321637"
+            },
+            {
+                color: "White",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_52f52631-54cd-434f-bc2c-339cb8f00a45_1000x.jpg?v=1657301868",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_b35751ad-6e90-4d1d-b22c-b4824e988ff0_1000x.jpg?v=1657301870",
+                imgIconURL: "https:////cdn.shopify.com/s/files/1/0667/0133/products/image_52f52631-54cd-434f-bc2c-339cb8f00a45_150x.jpg?v=1657301868"
+            },
+        ]
+    },
+    {
+        title: "ELMTS HOODIE",
+        description: "Men's Relaxed Interlock Knit Hoodie",
+        type: "hoodie",
+        gender: "men",
+        price: 75.00,
+        sizes: ["S", "M", "L", "XL"],
+        colorways: [
+            {
+                color: "Fudge Brown",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieFudgeBrown4_dc5919ba-7619-46df-b446-5d4dd8f78d60_1000x.jpg?v=1665438045",
+                imgSideURL:  "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieFudgeBrown2_ddcfbc63-4468-445e-91c3-fcc92ac564e9_1000x.jpg?v=1665438045",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieFudgeBrown4_dc5919ba-7619-46df-b446-5d4dd8f78d60_150x.jpg?v=1665438045",
+            },
+            {
+                color: "Oyster Grey",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieOysterGrey1_1000x.jpg?v=1665177593",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieOysterGrey3_1000x.jpg?v=1665177630",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieOysterGrey1_150x.jpg?v=1665177593",
+            },
+            {
+                color: "Harbor Navy",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieHarborNavy1_1000x.jpg?v=1665177169",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieHarborNavy3_1000x.jpg?v=1665177168",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/PulloverHoodieHarborNavy1_150x.jpg?v=1665177169",
+            },
+            {
+                color: "Barossa",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Men_sElements1001_1000x.jpg?v=1648077479",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Men_sElements1005_1000x.jpg?v=1650918304",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Men_sElements1001_150x.jpg?v=1648077479"
+            },
+        ]
+    },
+    {
+        title: "Swim Trunk 5.5inch",
+        description: "Men's Lined Swim Trunk",
+        type: "swimtrunk",
+        gender: "men",
+        price: 67.89,
+        sizes: ["S", "M", "L", "XL"],
+        colorways: [
+            {
+                color: "Coral / Light Blue",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Kierantsandshorts58_1_1000x.jpg?v=1657213890",
+                imgSideURL:  "https://cdn.shopify.com/s/files/1/0667/0133/products/Kierantsandshorts59_1_1000x.jpg?v=1657213945",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Kierantsandshorts58_1_150x.jpg?v=1657213890",
+            },
+            {
+                color: "Black",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Kierantsandshorts63_1000x.jpg?v=1657213776",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Kierantsandshorts64_1000x.jpg?v=1657213704",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/Kierantsandshorts63_150x.jpg?v=1657213776",
+            },
+            {
+                color: "Green / Black",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_31a3c701-1f38-4df9-9155-04c49eca6a86_1000x.jpg?v=1657213999",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_2c6b3ae0-afc1-44b4-8b9d-3c964e830ec5_1000x.jpg?v=1657214004",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_31a3c701-1f38-4df9-9155-04c49eca6a86_150x.jpg?v=1657213999",
+            },
+            {
+                color: "Orange / Blue",
+                imgURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_09c4a14c-cc29-48c5-bcf3-d229528a6ebe_1000x.jpg?v=1657213844",
+                imgSideURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_59c721ed-2035-47ea-81e9-956b76c476f8_1000x.jpg?v=1657213860",
+                imgIconURL: "https://cdn.shopify.com/s/files/1/0667/0133/products/image_09c4a14c-cc29-48c5-bcf3-d229528a6ebe_150x.jpg?v=1657213844"
+            },
+        ]
+    },
 ]
 
 export { data }
